130c2b4e8dff34e27fc6a819752ccfc15aff85d8
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-17 15:36:50 +0100

    agregada ruta a la landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/landing/landing').then((m) => m.Landing),
+  },
+];
