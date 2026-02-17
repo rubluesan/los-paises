5de7d692d9973f8024bf17acb2028dc261a8382e
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-17 21:41:01 +0100

    añadido lucide-icons y estilo general header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
+import {
+  LucideAngularModule,
+  Globe,
+  LogOut
+} from 'lucide-angular';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        Globe,
+        LogOut
+      }))],
 };
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
