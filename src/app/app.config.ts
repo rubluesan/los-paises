0740b47c39e6be418326cef1149de8db4996f04d
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-18 13:51:26 +0100

    fix: ahora se muestra correctamente activo el link de la ruta actual

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
-import { LucideAngularModule, Globe, LogOut, Home } from 'lucide-angular';
+import { LucideAngularModule, Globe, LogOut, Home, Binoculars } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,6 +20,7 @@ export const appConfig: ApplicationConfig = {
         Globe,
         LogOut,
         Home,
+        Binoculars,
       }),
     ),
   ],
