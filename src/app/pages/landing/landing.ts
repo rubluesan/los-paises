ef4a87ba88c8ede61799aff317f64b358d98a794
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-19 04:47:13 +0100

    add: servicios de title y meta para seo

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './landing.css',
 })
 export class Landing {
+  private titleService = inject(Title);
+  private metaService = inject(Meta);
 
+  ngOnInit() {
+    this.titleService.setTitle('Los Países | Comunidad Global de Viajeros');
+    this.metaService.updateTag({
+      name: 'description',
+      content:
+        'Únete a Los Países, la red social para viajeros. Descubre datos reales de todos los países y comparte tus valoraciones con el mundo.',
+    });
+  }
 }
