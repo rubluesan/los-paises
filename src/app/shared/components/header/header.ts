b8d0470b1090269b541d40dbbbac7e03f37f8741
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-19 21:52:18 +0100

    cambios para mayor accesibilidad

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,10 +1,11 @@
-import { Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-header',
-  imports: [LucideAngularModule, RouterLink, RouterLinkActive],
+  imports: [LucideAngularModule, RouterLink, RouterLinkActive, A11yModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
@@ -12,6 +13,13 @@ export class Header {
   private router: Router = inject(Router);
 
   isOpen = signal(false);
+
+  @HostListener('document:keydown.escape')
+  onEscape() {
+    if (this.isOpen()) {
+      this.toggleMenu();
+    }
+  }
 
   toggleMenu() {
     const open = !this.isOpen();
