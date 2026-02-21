e817a670b410cb2f75a7a8635b801b8dcd923363
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-21 04:06:30 +0100

    add: modelo con todos los datos que usaré

--- src/app/core/models/Country.ts
+++ src/app/core/models/Country.ts
@@ -2,5 +2,14 @@ export interface Country {
   name: {
     common: string;
   };
+  cca3: string;
+  capital: string[];
   region: string;
+  population: number;
+  flags: { png: string; alt: string };
+  translations: {
+    spa: {
+      common: string;
+    };
+  };
 }
