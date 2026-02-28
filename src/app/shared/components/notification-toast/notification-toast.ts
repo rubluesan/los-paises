acb4719c779675b94d9804570046e91bfc8a3be1
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-02-28 03:48:36 +0100

    add: componente de notificaciones

---
+++ src/app/shared/components/notification-toast/notification-toast.ts
@@ -0,0 +1,29 @@
+import { Component, input, output, signal } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-notification-toast',
+  imports: [LucideAngularModule],
+  templateUrl: './notification-toast.html',
+  styleUrl: './notification-toast.css',
+})
+export class NotificationToast {
+  message = input('');
+  isError = input(false);
+
+  onClose = output<void>();
+  isLeaving = signal(false);
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.startExit();
+    }, 3700);
+  }
+
+  private startExit() {
+    this.isLeaving.set(true);
+    setTimeout(() => {
+      this.onClose.emit();
+    }, 300);
+  }
+}
